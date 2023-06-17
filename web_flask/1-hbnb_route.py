33dfadc062c90dbd7d7169617c02c179a766d4c2
bsltan <califchichi@yahoo.com>
2023-06-17 10:46:24 +0300

    0task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-listens on 0.0.0.0:5000.
-"""
+"""Flask framework"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+def hello_world():
+    """return hello HBNB"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """return HBNB"""
     return "HBNB"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
